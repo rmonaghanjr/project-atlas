08f73f8786dd8e2ddf2b83f27777e5ebbc376d7b
rmonaghanjr <rmonagha@stevens.edu>
2023-07-19 21:21:25 -0400

    verify malloc

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include <stdlib.h>
+#include <string.h>
+
 int main() {
     // i am the bit master
     // bow before me
@@ -12,6 +15,24 @@ int main() {
     // this will be so much easier than rust
 
     DDRA |= (1<<PA4);
+
+    char* string = "hello world\n";
+    char* new_region = (char*) malloc(strlen(string));
+
+    memcpy(new_region, string, strlen(string));
+
+    // verify that malloc works because it didn't in rust
+    for (int i = 0; i < strlen(string); i++) {
+        if (string[i] == new_region[i]) {
+            PORTA |= (1<<PA4);
+            _delay_ms(100);
+
+            PORTA &= ~(1<<PA4);
+            _delay_ms(100);
+        }
+    }
+
+    // busy wait
     while(1) {
         PORTA |= (1<<PA4);
         _delay_ms(1000);
